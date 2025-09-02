d91f967e8ee5cae6e7136396d938b68cbb852484
useryejun12348888 <user@DESKTOP-ESIUD68>
2025-09-02 09:42:13 +0900

    first commit

---
+++ src/main/java/com/spring/app/Devops01Application.java
@@ -0,0 +1,13 @@
+package com.spring.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devops01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devops01Application.class, args);
+	}
+
+}
